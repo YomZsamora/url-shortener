f1d56c9f873089851cf2f2d4e5d15f4cdf11a6a0
yomZsamora <samaurah@gmail.com>
2026-09-19 19:57:18 +0300

    chore: close sequelize and redis connections after tests

--- src/tests/setupFilesAfterEnv.js
+++ src/tests/setupFilesAfterEnv.js
@@ -1,5 +1,7 @@
 afterAll(async () => {
     const sequelize = require('../configs/sequelize');
+    const redis = require('../configs/redis');
     await sequelize.close();
+    await redis.quit();
     jest.resetModules();
 });
