8b79d705391ca3dbf3771616275b52b1246aebbf
yomZsamora <samaurah@gmail.com>
2026-09-19 10:23:23 +0300

    feat: enhance links routes by adding list, get, update, and delete endpoints with corresponding middlewares

--- src/app/routes/links-routes.js
+++ src/app/routes/links-routes.js
@@ -1,9 +1,23 @@
 const { Router } = require('express');
-const { createLink } = require('../controllers/links-controller');
-const { createLinkMiddlewares } = require('../middlewares/links-middlewares');
+const {
+    createLink,
+    listLinks,
+    getLink,
+    updateLink,
+    deleteLink,
+} = require('../controllers/links-controller');
+const {
+    createLinkMiddlewares,
+    updateLinkMiddlewares,
+    listLinksMiddlewares,
+} = require('../middlewares/links-middlewares');
 
 const router = Router();
 
+router.get('/', listLinksMiddlewares, listLinks);
 router.post('/', createLinkMiddlewares, createLink);
+router.get('/:code', getLink);
+router.patch('/:code', updateLinkMiddlewares, updateLink);
+router.delete('/:code', deleteLink);
 
 module.exports = router;
